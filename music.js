6982e9bfeb50dd4409875f6d6d5f049458e2ef76
3itva <eitvilas12@gmail.com>
2019-05-21 21:35:00 +0200

    fixing files

--- music.js
+++ music.js
@@ -4,17 +4,24 @@ var playMusic = document.querySelector("#playBtn");
 var pauseMusic = document.querySelector("#pauseBtn")
 var myMusic = document.querySelector("#myMusic");
 
-playMusic.addEventListener("click", function(){
-    myMusic.play();
-    pauseMusic.style.display = "inline";
-    playMusic.style.display = "none";
-});
-pauseMusic.addEventListener("click", function(){
-    myMusic.pause();
-    pauseMusic.style.display = "none";
-    playMusic.style.display = "inline";
-});
+/* Event Listener! */
+playMusic.addEventListener("click", playfunc);
+pauseMusic.addEventListener("click", pausefunc);
 
+
+
+
+/* Functions begining! */
+function playfunc() {
+	myMusic.play();
+	pauseMusic.style.display = "inline";
+    playMusic.style.display = "none";
+}
+function pausefunc() {
+	myMusic.pause();
+	pauseMusic.style.display = "none";
+    playMusic.style.display = "inline";
+}
 // function checkVis() {
 //   if (myMusic.play()) {
 //     pauseMusic.style.display === "none";
